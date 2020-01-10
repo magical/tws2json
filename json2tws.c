ce0eb26a4c17cb6852eef5b52a0562c3a70048d1
Andrew Ekstedt <andrew.ekstedt@gmail.com>
2020-01-10 00:12:48 -0800

    [wip] json2tws

---
+++ json2tws.c
@@ -0,0 +1,113 @@
+/* tws2json.c: Convert a Tile World solution file to a JSON format.
+ * 
+ * Copyright © 2011 by Andrew Ekstedt, under the GNU General Public
+ * License. No warranty. See COPYING for details.
+ */
+
+#include <stddef.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "bstrlib.h"
+
+#include "solution.h"
+#include "fileio.h"
+#include "err.h"
+
+#include "version.h"
+
+#include "json.h"
+
+size_t read_callback(void *buf, size_t elsize, size_t nelem, void *fp) {
+	return fread(buf, elsize, nelem, fp);
+}
+
+bstring filereadfull(fileinfo *file) {
+	bstring str = bread(read_callback, file->fp);
+	if (!str) {
+		fileerr(file, "read error");
+	}
+	return str;
+}
+
+bool object_entry_eq(json_object_entry* entry, const char* str) {
+	int len = strlen(str);
+	return len == entry->name_length && strncmp(entry->name, str, len) == 0;
+}
+
+bool value_eq(json_value* v, const char* str) {
+	return v->type == json_string && strcmp(v->u.string.ptr, str) == 0;
+}
+
+json_value* find_entry(json_value* v, const char* key) {
+	if (v->type != json_object) {
+		return NULL;
+	}
+	for (long i = 0; i < v->u.object.length; i++) {
+		if (object_entry_eq(&v->u.object.values[i], key)) {
+			return v->u.object.values[i].value;
+		}
+	}
+}
+
+int doit(json_value *json) {
+	if (json->type != json_object) {
+		errmsg("error", "expected an object");
+		return 1;
+	}
+
+	json_value* class = find_entry(json, "class");
+	if (!class || !value_eq(class, "tws")) {
+		errmsg("error", "expected object to have class \"tws\"");
+		return 1;
+	}
+
+	json_value* solutions = find_entry(json, "solutions");
+	if (!solutions || solutions->type != json_array) {
+		errmsg("error", "no solutions");
+	}
+
+	for (long i = 0; i < solutions->u.array.length; i++) {
+		json_value* sol = solutions->u.array.values[i];
+		json_value* moves = find_entry(sol, "moves");
+		if (moves && moves->type == json_string) {
+			printf("%s\n", moves->u.string.ptr);
+		}
+	}
+
+	return 0;
+}
+
+int main(int argc, char** argv) {
+	fileinfo file;
+	bstring data;
+	json_value* json;
+
+	if (argc < 2) {
+	    fprintf(stderr, "usage: json2tws file.json\n");
+	    return 1;
+	}
+
+	if (!fileopen(&file, argv[1], "rb", "file error")) {
+		return 1;
+	}
+
+	data = filereadfull(&file);
+	fileclose(&file, "file error");
+
+	if (!data) {
+		return 1;
+	}
+
+	json = json_parse(bdata(data), blength(data));
+	if (!json) {
+		errmsg("", "error parsing json");
+		return 1;
+	}
+
+	int status = doit(json);
+
+	json_value_free(json);
+	return status;
+}
---
+++ json2txt.py
@@ -0,0 +1,280 @@
+#!/usr/bin/env python3
+import io
+import json
+import sys
+
+def main():
+    j = json.load(sys.stdin)
+    for sol in j['solutions']:
+        if 'moves' in sol:
+            print(sol['moves'])
+            print(' '.join(parsemoves(sol['moves'])))
+
+DIGITS = '0123456789'
+UPPERMOVES = "UDLR"
+LOWERMOVES = "udlr"
+WAIT = ",."
+MOUSE = "*"
+EOF = ""
+
+# states
+new_move = 1
+digit_or_move  = 2
+mouse  = 6
+mouse1a = 8
+mouse1b = 9 
+mouse2a =10
+mouse2b =11
+moveupper = 12
+movelower = 13
+diagonalupper = 14
+diagonallower = 15
+
+
+def parsemoves(s):
+    r = io.StringIO(s)
+    moves = []
+    count = 0
+    move = ""
+    state = new_move
+    column = 0
+
+    def flush_move():
+        nonlocal move
+        for _ in range(count):
+            moves.append(move)
+        move = ""
+
+    while True:
+        if state == 'error':
+            print("error at column", column)
+            break
+
+        if state == 'flush':
+            flush_move()
+            state = new_move
+
+        column += 1
+
+        c = r.read(1)
+        #print(c, state)
+
+#    new_move -> digit_or_move [label="1-9"];
+#    new_move -> mouse [label="*"];
+#    new_move -> moveupper [label="UDLR"];
+#    new_move -> movelower [label="udlr"];
+#    new_move -> new_move [label="<space>"];
+#
+        if state == new_move:
+            count = 1
+            if c in DIGITS:
+                count = DIGITS.index(c)
+                state = digit_or_move
+            elif c == MOUSE:
+                move = '*'
+                state = mouse
+            elif c in UPPERMOVES:
+                move = c
+                state = moveupper
+            elif c in LOWERMOVES:
+                move = c
+                state = movelower
+            elif c in WAIT:
+                move = c
+                state = 'flush'
+            elif c == ' ':
+                pass
+            elif c == EOF:
+                pass
+            else:
+                state = 'error'
+#    digit_or_move -> digit_or_move [label="0-9"];
+#    digit_or_move -> moveupper [label="UDLR"];
+#    digit_or_move -> movelower [label="udlr"];
+#    digit_or_move -> new_move [label=", .", style=bold];
+        elif state == digit_or_move:
+            if c in DIGITS:
+                count = count*10 + DIGITS.index(c)
+            elif c in UPPERMOVES:
+                move = c
+                state = moveupper
+            elif c in LOWERMOVES:
+                move = c
+                state = movelower
+            elif c in WAIT:
+                move = c
+                state = 'flush'
+            else:
+                state = 'error'
+
+#    moveupper -> diagonalupper [label="+"];
+#    moveupper -> digit_or_move [label="1-9", style=dashed];
+#    moveupper -> mouse [label="*", style=dashed];
+#    moveupper -> moveupper [label="UDLR", style=dashed];
+#    moveupper -> movelower [label="udlr", style=dashed];
+        elif state == moveupper:
+            if c == '+':
+                state = diagonalupper
+            else:
+                flush_move()
+
+                count = 1
+                if c in DIGITS:
+                    count = DIGITS.index(c)
+                    state = digit_or_move
+                elif c == MOUSE:
+                    state = mouse
+                elif c in UPPERMOVES:
+                    move = c
+                    state = moveupper
+                elif c in LOWERMOVES:
+                    move = c
+                    state = movelower
+                elif c in WAIT:
+                    move = c
+                    state = 'flush'
+                elif c == ' ':
+                    state = new_move
+                elif c == EOF:
+                    flush_move()
+                else:
+                    state = 'error'
+#
+#    movelower -> diagonallower [label="+"];
+#    movelower -> digit_or_move [label="1-9", style=dashed];
+#    movelower -> mouse [label="*", style=dashed];
+#    movelower -> moveupper [label="UDLR", style=dashed];
+#    movelower -> movelower [label="udlr", style=dashed];
+        elif state == movelower:
+            if c == '+':
+                state = diagonallower
+            else:
+                flush_move()
+
+                count = 1
+                if c in DIGITS:
+                    count = DIGITS.index(c)
+                    state = digit_or_move
+                elif c == MOUSE:
+                    state = mouse
+                elif c in UPPERMOVES:
+                    move = c
+                    state = moveupper
+                elif c in LOWERMOVES:
+                    move = c
+                    state = movelower
+                elif c in WAIT:
+                    move = c
+                    state = 'flush'
+                elif c == ' ':
+                    state = new_move
+                elif c == EOF:
+                    flush_move()
+                else:
+                    state = 'error'
+
+#    diagonalupper -> new_move [label="UDLR", style=bold];
+        elif state == diagonalupper:
+            if c in UPPERMOVES:
+                move += c
+                state = 'flush'
+            else:
+                state = 'error'
+
+#    diagonallower -> new_move [label="udlr", style=bold];
+        elif state == diagonallower:
+            if c in LOWERMOVES:
+                move += c
+                state = 'flush'
+            else:
+                state = 'error'
+
+#    mouse -> new_move [label="."];
+#    mouse -> mouse1a [label="2-9"];
+#    mouse -> mouse1b [label="UDLR"];
+        elif state == mouse:
+            x = 0
+            y = 0
+            digit1 = 0
+            digit2 = 0
+            dir1 = None
+            dir2 = None
+            move = "*"
+            if c == '.':
+                x = 0
+                y = 0
+                state = 'flush'
+            elif c in DIGITS:
+                digit1 = int(c)
+                state = mouse1a
+            elif c in UPPERMOVES:
+                digit1 = 1
+                dir1 = c
+                state = mouse1b
+            else:
+                state = 'error'
+
+#    mouse1a -> mouse1b [label="UDLR"];
+        elif state == mouse1a:
+            if c in UPPERMOVES:
+                dir1 = c
+                state = mouse1b
+            else:
+                state = 'error'
+        
+
+#    mouse1b -> mouse2a [label=";"];
+#    mouse1b -> digit_or_move [label="1-9", style=dashed];
+#    mouse1b -> mouse [label="*", style=dashed];
+#    mouse1b -> moveupper [label="UDLR", style=dashed];
+#    mouse1b -> movelower [label="udlr", style=dashed];
+        elif state == mouse1b:
+            if c == ';':
+                state = mouse2a
+            else:
+                flush_move()
+
+                count = 1
+                if c in DIGITS:
+                    count = DIGITS.index(c)
+                    state = digit_or_move
+                elif c == MOUSE:
+                    state = mouse
+                elif c in UPPERMOVES:
+                    move = c
+                    state = moveupper
+                elif c in LOWERMOVES:
+                    move = c
+                    state = movelower
+                elif c in WAIT:
+                    move = c
+                    state = 'flush'
+                elif c == ' ':
+                    state = new_move
+                elif c == EOF:
+                    flush_move()
+                else:
+                    state = 'error'
+#
+#    // *L;2U
+#    mouse2a -> mouse2b [label="2-9"];
+#    mouse2a -> new_move [label="UDLR", style=bold];
+        elif state == mouse2a:
+            if c in DIGITS:
+                digit2 = int(c)
+                state = mouse2b
+            elif c in UPPERMOVES:
+                digit2 = 1
+                dir2 = c
+                state = 'flush'
+            else:
+                state = 'error'
+
+        if not c:
+            break
+            
+
+    return moves
+
+
+main()
